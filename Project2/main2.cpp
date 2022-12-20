0407b0ae0bb5867841ce4de9b2821d6b2cf3a332
MURFY AIMEE <pythonsenior@outlook.com>
2022-12-20 08:39:34 -1000

    Project2

---
+++ Project2/main2.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Acc_Sys {
+    string welcome;
+    int ID;
+    int number;
+    int time;
+};
+
+struct User_Accsys
+{
+    void Check_user(string User_password, string User_login, string User_special_code)
+    {
+        string login = "sairanfancho";
+        string password = "portretMARKOVA";
+
+        cout << "Enter login: ";
+        cin >> User_login;
+
+        cout << "Enter password: ";
+        cin >> User_password;
+
+        if (User_login == login && User_password == password)
+        {
+            cout << "You're welcome" << endl;
+        }
+        else
+        {
+            cout << "Error" << endl;
+
+            exit(0);
+        }
+    };
+};
+
+struct Phone_Number
+{
+    string phone_number;
+};
+
+int main()
+{
+    Acc_Sys as;
+    as.welcome = "Welcome";
+    as.ID = 716803474;
+    as.number = 2206;
+    as.time = 6;
+    cout << as.welcome << endl;
+    cout << "ID: " << as.ID << endl;
+    cout << "Number: " << as.number << endl;
+    cout << "Time: " << as.time << endl;
+
+    User_Accsys ua;
+    string login;
+    string password;
+    ua.Check_user(login, password);
+
+    Phone_Number pn;
+    pn.phone_number;
+
+    cout << "Enter Phone Numder: ";
+    cin >> pn.phone_number;
+
+    cin.get();
+    return 0;
+}
